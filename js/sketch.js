254517dcbf4f67c2df1e8d8775ed413e38a8755c
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-12-02 11:30:37 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,26 @@
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    background(220);
+    colorMode(HSB)
+
+    for(var i = 0; i < 150; i++) {
+        circles(); 
+    }
+    
 }
 
 function draw() {
-    background(220);
+    
+}
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+function circles() {
+
+    var x = random(width);
+    var y = random(height);
+    var d = random (5, 150);
+    var hue = random(120, 255)
+
+    fill(hue, 255, 255, 0.1);
+    circle(x, y, d);
 }
